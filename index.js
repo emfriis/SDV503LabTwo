4a32ef5476ef7dcc15ec88d61e79ec10473e66a6
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-03-10 12:40:48 +1300

    Added code and comments

---
+++ index.js
@@ -0,0 +1,8 @@
+let numFirst = 5; // Declares variable numFirst with value 5.
+let numSecond = 7; // Declares variable numSecond with value 7.
+let numThird = 12; // Declares variable numThrid with value 12.
+let numFourth = 15; // Declares variable numFourth with value 15.
+
+console.log('The initial numbers are: ', numFirst, ", ", numSecond, ", ", numThird, "and ", numFourth); // Prints the initial numbers formatted with strings for clarity.
+console.log("\nThe sum of the cubes of the inital numbers is:"); // Prints preceding text for result on a new line.
+console.log((numFirst ** 3) + (numSecond ** 3) + (numThird ** 3) + (numFourth **3)); // Prints the result of the sum of the cubes of the initial numbers.
